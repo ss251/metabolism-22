7991b163e4fc4c1f34a33efca7a922d944dd57fe
ss251 <ss251@uw.edu>
2022-08-07 13:18:00 -0500

    remove protocol, api and home page

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -13,13 +13,6 @@ export const Header = () => {
       </div>
       <div className="z-10 bg-black fixed top-12 sm:top-0 px-4 py-w border-2 border-solid border-white flex flex-row w-fit space-x-4">
         <Link
-          href="/"
-        >
-          <a className="hover:text-[#f53bc3]">
-          HOME
-          </a>
-        </Link>
-        <Link
           href="/mint"
         >
           <a className="hover:text-[#f53bc3]">
@@ -34,26 +27,13 @@ export const Header = () => {
           </a>
         </Link>
         <Link
-          href="/indexer"
-        >
-          <a className="hover:text-[#f53bc3]">
-            API
-          </a>
-        </Link>
-        <Link
           href="/create"
         >
           <a className="hover:text-[#f53bc3]">
             CREATE
           </a>
         </Link>
-        <Link
-          href="/protocol"
-        >
-          <a className="hover:text-[#f53bc3]">
-            PROTOCOL
-          </a>
-        </Link>
+        
       </div>
     </div>
   )
